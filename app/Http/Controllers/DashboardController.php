6e4bd660a20327108b7847103d794424dc813a75
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-01-28 15:52:05 +0500

    Info on dashbaord

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BuildCategory;
+use App\Models\Gallery;
+use App\Models\Motherboard;
+use App\Models\Package;
+use App\Models\Processor;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard');
+        return view('dashboard', [
+            'cpus' => Processor::count(),
+            'cpu_price' => Processor::sum('price'),
+
+            'motherboards' => Motherboard::count(),
+            'motherboard_price' => Motherboard::sum('price'),
+
+            'build_category' => BuildCategory::count(),
+            'builds' => Gallery::count(),
+
+            'packages' => Package::count(),
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,40 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <div class="max-w-6xl mx-auto sm:px-6 p-6">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6">
+                <div class="p-6 text-gray-900 grid grid-cols-3 gap-6">
+                    <div class="bg-gray-100 border border-gray-100 rounded-lg p-6">
+                        <h2 class="font-semibold text-2xl">Total CPU</h2>
+                        <p>{{ $cpus }}</p>
+                    </div>
+                    <div class="bg-gray-100 border border-gray-100 rounded-lg p-6">
+                        <h2 class="font-semibold text-2xl">Total Worth Of CPU</h2>
+                        <p>€{{ $cpu_price }}</p>
+                    </div>
+                    <div class="bg-gray-100 border border-gray-100 rounded-lg p-6">
+                        <h2 class="font-semibold text-2xl">Total Motherboards</h2>
+                        <p>{{ $motherboards }}</p>
+                    </div>
+                    <div class="bg-gray-100 border border-gray-100 rounded-lg p-6">
+                        <h2 class="font-semibold text-2xl">Worth of Motherboards</h2>
+                        <p>€{{ $motherboard_price }}</p>
+                    </div>
+                    <div class="bg-gray-100 border border-gray-100 rounded-lg p-6">
+                        <h2 class="font-semibold text-2xl">Total Packages</h2>
+                        <p>{{ $packages }}</p>
+                    </div>
+                </div>
+
+                <div class="p-6 text-gray-900 grid grid-cols-3 gap-6 border-t border-gray-200">
+                    <div class="bg-gray-100 border border-gray-100 rounded-lg p-6">
+                        <h2 class="font-semibold text-2xl">Build Categories</h2>
+                        <p>{{ $build_category }}</p>
+                    </div>
+                    <div class="bg-gray-100 border border-gray-100 rounded-lg p-6">
+                        <h2 class="font-semibold text-2xl">Builds</h2>
+                        <p>{{ $builds }}</p>
+                    </div>
                 </div>
             </div>
         </div>
